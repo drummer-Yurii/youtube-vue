ff7e53a7e5d909f708d6d0107ef1dd9ad5620c57
Yurii Rybachok <drummer2009@ukr.net>
2023-05-25 19:03:07 +0300

    create project and add components

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <TheHeader />
+    <TheSidebarSmall />
+    <TheSidebar />
+    <TheSidebarMobile />
+    <TheCatigories />
+    <TheVideos />
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheSidebarSmall from './components/TheSidebarSmall.vue';
+import TheSidebar from './components/TheSidebar.vue';
+import TheSidebarMobile from './components/TheSidebarMobile.vue';
+import TheCatigories from './components/TheCatigories.vue';
+import TheVideos from './components/TheVideos.vue';
+
+export default {
+  components: {
+      TheHeader,
+      TheSidebarSmall,
+      TheSidebar,
+      TheSidebarMobile,
+      TheCatigories,
+      TheVideos
+  }
+}
+</script>
