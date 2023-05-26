ffb48a0bfdb30bcbf466a91e1cf1e7ef6a489845
Yurii Rybachok <drummer2009@ukr.net>
2023-05-26 10:12:37 +0300

    create ButtonLogin component

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -29,10 +29,7 @@
             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"></path></svg>
             <TheDropdownSettings />
         </button>
-        <button class="focus:outline-none flex items-center whitespace-nowrap px-2 py-1 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm">
-            <svg class="mr-2 w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-            Sign In
-        </button>
+        <ButtonLogin />
         </div>
     </header>
 </template>
@@ -42,13 +39,15 @@ import TheDropdownApps from './TheDropdownApps.vue';
 import TheDropdownSettings from './TheDropdownSettings.vue';
 import LogoMain from './LogoMain.vue';
 import TheSearch from './TheSearch.vue';
+import ButtonLogin from './ButtonLogin.vue'
 
 export default {
     components: {
         TheDropdownApps,
         TheDropdownSettings,
         LogoMain,
-        TheSearch
+        TheSearch,
+        ButtonLogin
     }
 }
 </script>
