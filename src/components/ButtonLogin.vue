074d053bf5f29dc0663eddc75d122d0c7023f680
Yurii Rybachok <drummer2009@ukr.net>
2023-05-26 19:20:01 +0300

    reusable components for sidebar

--- src/components/ButtonLogin.vue
+++ src/components/ButtonLogin.vue
@@ -1,7 +1,7 @@
 <template>
     <button
-        class="focus:outline-none flex items-center whitespace-nowrap px-2 py-1 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm">
-        <BaseIcon name="user" class="mr-2 w-7 h-7" />
+        :class="classes">
+        <BaseIcon name="userCircle" class="mr-2 w-7 h-7" />
         Sign In
     </button>
 </template>
@@ -12,6 +12,14 @@ import BaseIcon from './BaseIcon.vue';
 export default {
     components: {
         BaseIcon
+    },
+    computed: {
+        classes() {
+            return (
+                this.$attrs.class || 
+                'focus:outline-none flex items-center whitespace-nowrap px-2 py-1 text-sm text-blue-700 uppercase border border-blue-700 rounded-sm'
+            )
+        }
     }    
 }
 </script>
