3f30e9ed29f582932f00087404f1284afd288770
Yurii Rybachok <drummer2009@ukr.net>
2023-05-29 15:03:24 +0300

    show appearance menu

--- src/components/TheDropdownSettings.vue
+++ src/components/TheDropdownSettings.vue
@@ -23,22 +23,14 @@
             tabindex="-1"
             :class="dropdownClasses"
         >
-            <section class="py-2 border-b">
-                <ul>
-                    <DropdownSettingsListItem 
-                        v-for="listItem in listItems.slice(0, 8)"
-                        :key="listItem.label"
-                        :label="listItem.label"
-                        :icon="listItem.icon"
-                        :with-sub-menu="listItem.withSubMenu"
-                    />
-                </ul>
-            </section>
-            <section class="py-2">
-                <ul>
-                    <DropdownSettingsListItem :label="listItems[8].label" :with-sub-menu="listItems[8].withSubMenu" />
-                </ul>
-            </section>
+            <TheDropdownSettingsMain 
+                v-if="selectedMenu === 'main'" 
+                @select-menu="showSelectedMenu" 
+            />
+            <TheDropdownSettingsAppearance 
+                v-else-if="selectedMenu === 'appearance'" 
+                @select-menu="showSelectedMenu"
+            />
         </div>
     </transition>
     </div>
@@ -47,22 +39,25 @@
 <script>
 import BaseIcon from './BaseIcon.vue';
 import BaseTooltip from './BaseTooltip.vue';
-import DropdownSettingsListItem from './DropdownSettingsListItem.vue';
+import TheDropdownSettingsMain from './TheDropdownSettingsMain.vue';
+import TheDropdownSettingsAppearance from './TheDropdownSettingsAppearance.vue';
 
 export default {
     components: {
-        DropdownSettingsListItem,
         BaseIcon,
-        BaseTooltip
+        BaseTooltip,
+        TheDropdownSettingsMain,
+        TheDropdownSettingsAppearance
     },
 
     data() {
         return {
             isOpen: false,
+            selectedMenu: 'main',
             dropdownClasses: [
                 'z-10', 
                 'absolute', 
-                'top-9', 
+                'top-9',  
                 '-right-full', 
                 'sm:right-0', 
                 'bg-white', 
@@ -70,53 +65,6 @@ export default {
                 'border', 
                 'border-t-0', 
                 'focus:outline-none'
-            ],
-            listItems: [
-                {
-                    label: 'Appearance: Light',
-                    icon: 'sun',
-                    withSubMenu: true
-                },
-                {
-                    label: 'Language: English',
-                    icon: 'translate',
-                    withSubMenu: true
-                },
-                {
-                    label: 'Location: Ukraine',
-                    icon: 'globeAlt',
-                    withSubMenu: true
-                },
-                {
-                    label: 'Settings',
-                    icon: 'cog',
-                    withSubMenu: false
-                },
-                {
-                    label: 'Your data in YouTube',
-                    icon: 'shieldCheck',
-                    withSubMenu: false
-                },
-                {
-                    label: 'Help',
-                    icon: 'questionMarkCircle',
-                    withSubMenu: false
-                },
-                {
-                    label: 'Send feedback',
-                    icon: 'chatAlt',
-                    withSubMenu: false
-                },
-                {
-                    label: 'Keyboard shortcust',
-                    icon: 'calculator',
-                    withSubMenu: false
-                },
-                {
-                    label: 'Restricted Mode: Off',
-                    icon: null,
-                    withSubMenu: true
-                },
             ]
         }
     },
@@ -133,6 +81,12 @@ export default {
                 this.isOpen = false
             }
         });
+    },
+
+    methods: {
+        showSelectedMenu (selectedMenu) {
+            this.selectedMenu = selectedMenu
+        }
     },
 }
 </script>
