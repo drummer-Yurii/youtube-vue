f82c7f0b506b3c7476af693906a0d44e06358d87
Yurii Rybachok <drummer2009@ukr.net>
2023-05-31 16:50:33 +0300

    atomic modefier for v-model in search

--- src/components/TheSearchInput.vue
+++ src/components/TheSearchInput.vue
@@ -5,12 +5,12 @@
             placeholder="Search"
             :class="classes"
             :value="query"
-            @input="$emit('update:query', $event.target.value)"
+            @input="updateQuery($event.target.value)"
         >
         <button 
             class="absolute top-0 right-0 h-full px-3 focus:outline-none"
             v-show="query"
-            @click="$emit('update:query', '')"
+            @click="updateQuery('')"
         >
             <BaseIcon name="x" class="w-5 h-5" />
         </button>
@@ -50,6 +50,12 @@ export default {
         if (window.innerWidth < 640) {
             this.$el.focus()
         };
+    },
+
+    methods: {
+        updateQuery (query) {
+            this.$emit('update:query', query)
+        }
     },
 }
 </script>
